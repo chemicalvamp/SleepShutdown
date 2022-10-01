5f00fb087fe229dc1e3ecf3886e3e272a801a160
Keith <vampyreaddiction@gmail.com>
2022-10-01 07:46:20 -0500

    Add files via upload

---
+++ SleepShutdown/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SleepShutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.formUpdate = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.directoryLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.shutdownLabel = new System.Windows.Forms.Label();
+            this.activityTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // formUpdate
+            // 
+            this.formUpdate.Enabled = true;
+            this.formUpdate.Interval = 500;
+            this.formUpdate.Tick += new System.EventHandler(this.formUpdate_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monitoring:";
+            // 
+            // directoryLabel
+            // 
+            this.directoryLabel.AutoSize = true;
+            this.directoryLabel.Location = new System.Drawing.Point(77, 9);
+            this.directoryLabel.Name = "directoryLabel";
+            this.directoryLabel.Size = new System.Drawing.Size(0, 13);
+            this.directoryLabel.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Activity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Shutdown in:";
+            // 
+            // shutdownLabel
+            // 
+            this.shutdownLabel.AutoSize = true;
+            this.shutdownLabel.Location = new System.Drawing.Point(100, 59);
+            this.shutdownLabel.Name = "shutdownLabel";
+            this.shutdownLabel.Size = new System.Drawing.Size(49, 13);
+            this.shutdownLabel.TabIndex = 4;
+            this.shutdownLabel.Text = "99:99:99";
+            // 
+            // activityTime
+            // 
+            this.activityTime.AutoSize = true;
+            this.activityTime.Location = new System.Drawing.Point(100, 46);
+            this.activityTime.Name = "activityTime";
+            this.activityTime.Size = new System.Drawing.Size(68, 13);
+            this.activityTime.TabIndex = 5;
+            this.activityTime.Text = "99:99:99 PM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 80);
+            this.Controls.Add(this.activityTime);
+            this.Controls.Add(this.shutdownLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.directoryLabel);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "SleepShutdown";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer formUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label directoryLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label shutdownLabel;
+        private System.Windows.Forms.Label activityTime;
+    }
+}
+
---
+++ SleepShutdown/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace SleepShutdown
+{
+    public partial class Form1 : Form
+    {
+        string watchDirectory = "";
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        DateTime lastActivity;
+        DateTime shutdownTime;
+        double activityThreshold = 1.00;
+        FileSystemWatcher fsw;
+        string commandArg = "shutdown /s";
+
+        private UserInput input;
+
+        public Form1(String[] args)
+        {
+            double timer;
+            InitializeComponent();
+            try
+            {
+                double.TryParse(args[0], out timer);
+                if (timer > 0)
+                    activityThreshold = timer;
+                if (args[1] != null)
+                    watchDirectory = args[1];
+            }
+            catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            input = new UserInput();
+            input.KeyBoardKeyPressed += anyActivity;
+            input.MouseMoved += anyActivity;
+
+            if (watchDirectory != "")
+            {
+                fsw = new FileSystemWatcher(Environment.ExpandEnvironmentVariables(watchDirectory));
+                directoryLabel.Text = watchDirectory;
+                fsw.Changed += new FileSystemEventHandler(anyActivity);
+                fsw.Created += new FileSystemEventHandler(anyActivity);
+                fsw.Deleted += new FileSystemEventHandler(anyActivity);
+                fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                fsw.IncludeSubdirectories = true;
+                fsw.EnableRaisingEvents = true;
+            }
+            else
+            {
+                fsw = new FileSystemWatcher(Environment.ExpandEnvironmentVariables(baseDirectory));
+                directoryLabel.Text = baseDirectory;
+                fsw.Changed += new FileSystemEventHandler(anyActivity);
+                fsw.Created += new FileSystemEventHandler(anyActivity);
+                fsw.Deleted += new FileSystemEventHandler(anyActivity);
+                fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                fsw.IncludeSubdirectories = true;
+                fsw.EnableRaisingEvents = true;
+            }
+        }
+
+        private void anyActivity(object sender, FileSystemEventArgs e) // Filesystem activity
+        {
+            lastActivity = DateTime.Now;
+            shutdownTime = lastActivity.AddHours(activityThreshold);
+        }
+
+        void anyActivity(object sender, EventArgs e) // UserInput activity
+        {
+            lastActivity = DateTime.Now;
+            shutdownTime = lastActivity.AddHours(activityThreshold);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            fsw.Dispose();
+            input.Dispose();
+        }
+
+        private void formUpdate_Tick(object sender, EventArgs e)
+        {
+            activityTime.Text = lastActivity.ToLongTimeString();
+            TimeSpan temp1 = TimeSpan.FromSeconds((shutdownTime - DateTime.Now).TotalSeconds);
+            TimeSpan temp2 = new TimeSpan(temp1.Hours, temp1.Minutes, temp1.Seconds);
+            shutdownLabel.Text = temp2.ToString();
+
+            if (DateTime.Compare(shutdownTime, DateTime.Now) <= 0)
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                startInfo.WorkingDirectory = @"C:\Windows\System32";
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/user:Administrator \"cmd /C " + commandArg + "\"";
+                startInfo.CreateNoWindow = false;
+                startInfo.ErrorDialog = false;
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardError = true;
+                startInfo.RedirectStandardOutput = true;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+        }
+    }
+
+    public class UserInput : IDisposable
+    {
+        public event EventHandler<EventArgs> KeyBoardKeyPressed;
+        public event EventHandler<EventArgs> MouseMoved;
+
+        public delegate IntPtr HookDelegate(int Code, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookDelegate lpfn, IntPtr hmod, int dwThreadId);
+
+        private HookDelegate keyBoardDelegate;
+        private HookDelegate mouseDelegate;
+        private IntPtr keyBoardHandle;
+        private IntPtr mouseHandle;
+        private const int WH_KEYBOARD_LL = 13; // Global only
+        private const int WH_MOUSE_LL = 14; // Global only
+        private bool disposed;
+
+        public UserInput()
+        {
+            keyBoardDelegate = KeyboardHookDelegate;
+            mouseDelegate = MouseHookDelegate;
+            keyBoardHandle = SetWindowsHookEx(WH_KEYBOARD_LL, keyBoardDelegate, IntPtr.Zero, 0);
+            mouseHandle = SetWindowsHookEx(WH_MOUSE_LL, mouseDelegate, IntPtr.Zero, 0);
+        }
+
+        private IntPtr KeyboardHookDelegate(int Code, IntPtr wParam, IntPtr lParam)
+        {
+            if (Code < 0)
+                return CallNextHookEx(keyBoardHandle, Code, wParam, lParam);
+
+            if (KeyBoardKeyPressed != null)
+                KeyBoardKeyPressed(this, new EventArgs());
+
+            return CallNextHookEx(keyBoardHandle, Code, wParam, lParam);
+        }
+
+        private IntPtr MouseHookDelegate(int Code, IntPtr wParam, IntPtr lParam)
+        {
+            if (Code < 0)
+                return CallNextHookEx(mouseHandle, Code, wParam, lParam);
+
+            if (MouseMoved != null)
+                MouseMoved(this, new EventArgs());
+
+            return CallNextHookEx(mouseHandle, Code, wParam, lParam);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (keyBoardHandle != IntPtr.Zero)
+                    UnhookWindowsHookEx(keyBoardHandle);
+                disposed = true;
+            }
+        }
+
+        ~UserInput()
+        {
+            Dispose(false);
+        }
+    }
+}
